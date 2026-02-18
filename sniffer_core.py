32017bbb6b96920f30a109e3cb7bb0f4a777268d
Arch-x86 <ankitthebe571@gmail.com>
2026-02-18 14:42:21 +0545

    added scapy dependency check and  conditional import

--- sniffer_core.py
+++ sniffer_core.py
@@ -6,4 +6,10 @@ from typing import Optional, Callable
 
 logger=logging.getLogger(__name__)
 
+try:
+    from scapy.all import sniff, Ether, IP, IPv6, TCP, UDP, ICMP, DNS, DNSQR, ARP, Raw
+    SCAPY_OK = True
+except ImportError:
+    SCAPY_OK = False
+    logger.warning("Scapy not installed. Run: pip install scapy")
 
