60e520388ecf54b4797ae8bd9635c08f44732d28
Arch-x86 <ankitthebe571@gmail.com>
2026-02-18 15:08:10 +0545

    Added  common port-to-service mapping and TCP flag abbreviations

--- sniffer_core.py
+++ sniffer_core.py
@@ -13,3 +13,12 @@ except ImportError:
     SCAPY_OK = False
     logger.warning("Scapy not installed. Run: pip install scapy")
 
+SERVICES = {
+    21: "FTP", 22: "SSH", 23: "Telnet", 25: "SMTP", 53: "DNS",
+    80: "HTTP", 110: "POP3", 143: "IMAP", 443: "HTTPS",
+    3306: "MySQL", 3389: "RDP", 8080: "HTTP-alt",
+}
+
+TCP_FLAGS = {"F": "FIN", "S": "SYN", "R": "RST", "P": "PSH", "A": "ACK", "U": "URG"}
+
+
