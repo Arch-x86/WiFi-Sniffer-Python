432a53ca9fad355ae2a3629707d330ab2a566f20
Arch-x86 <ankitthebe571@gmail.com>
2026-02-19 23:11:30 +0545

    added protocol color mapping and treeview column definitions

--- gui.py
+++ gui.py
@@ -9,3 +9,21 @@ from typing import Optional
 from sniffer_core import Sniffer, get_interfaces
 
 logger = logging.getLogger(__name__)
+
+PROTO_COLORS = {
+    "TCP":  "#e8f4fd",
+    "HTTP": "#fff3cd",
+    "UDP":  "#f3e5f5",
+    "DNS":  "#e8f5e9",
+    "ICMP": "#fce4ec",
+    "ARP":  "#fff8e1",
+}
+
+COLUMNS = [
+    ("time",     "Time",        90),
+    ("protocol", "Protocol",    72),
+    ("src",      "Source",     170),
+    ("dst",      "Destination",170),
+    ("length",   "Len",         52),
+    ("info",     "Info",       380),
+]
