64e11ff4403202f5b4cbe25cb1118c8dbf9859c8
Arch-x86 <ankitthebe571@gmail.com>
2026-02-21 21:21:10 +0545

    added ip viewer

---
+++ ip-viewer.py
@@ -0,0 +1,7 @@
+from scapy.all import get_if_list, get_if_addr, conf
+for iface in get_if_list():
+    try:
+        ip = get_if_addr(iface)
+        print(f"{iface}  →  {ip}")
+    except:
+        print(f"{iface}  →  (no IP)")
