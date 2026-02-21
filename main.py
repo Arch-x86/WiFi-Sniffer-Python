2ecf9f4e23e0bbac462232d3e3c5beedf7f39f3f
Arch-x86 <ankitthebe571@gmail.com>
2026-02-21 20:48:20 +0545

    added entry point and CLI argument support

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import argparse
 import logging
 import sys
+
+def main():
+    parser = argparse.ArgumentParser(description="WiFi / Packet Sniffer")
+    parser.add_argument("--no-gui", action="store_true", help="CLI mode")
+    parser.add_argument("--iface", "-i", default=None, help="Network interface")
+    parser.add_argument("--bpf", "-f", default="", help="BPF filter (e.g. 'tcp port 80')")
+    parser.add_argument("--test", action="store_true", help="Run unit tests")
+    parser.add_argument("--debug", action="store_true", help="Debug logging")
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.DEBUG if args.debug else logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    )
